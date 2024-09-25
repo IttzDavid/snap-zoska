c45d41afeae6185d2e25dc88e2d2e774854530c4
IttzDavid <davidokokosik@gmail.com>
2024-09-25 08:15:30 +0200

    initial

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,16 @@
+
+// src/app/(home)/page.tsx
+
+
+import Typography from "@mui/material/Typography";
+
+export const metadata = { title : 'Domov | David IG'};
+
+export default function Home() {
+
+  return (
+
+      <Typography> Domovská stránka</Typography>
+
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+// src/app/not-found.tsx
+
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export const metadata = { title : '404 | David IG'};
+
+export default function TermsConditions() {
+
+  return (
+    <Container>
+        <Typography> Nič tu nemáme :c </Typography>
+    </Container>    
+  );
+}
